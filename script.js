8efbabc334f22a0ac1baa9e143deef899d2ff46e
arshiya-rezaie <arshiyarezaie468@gmail.com>
2022-07-31 06:50:36 -0700

    arshi

---
+++ script.js
@@ -0,0 +1,22 @@
+var btnsub=document.querySelector(".sub");
+
+btnsub.addEventListener("click",()=>{
+
+
+
+
+
+var user=document.querySelector(".user").value;
+var pass=document.querySelector(".pass").value;
+var mail=document.querySelector(".mail").value;
+
+
+var username="arshia";
+var password="123";
+var email="arshia123@gmail.com";
+
+
+if(user==username && pass==password && mail==email )
+alert("khosh amadid")
+else(alert("dobre emthn konid"))
+});
